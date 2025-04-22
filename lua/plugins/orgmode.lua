4fe805411e4101e04ce8ed522eb031d18d6756eb
Sludge <alicembrooks@protonmail.com>
2025-04-22 23:55:52 +0100

    Add notes template

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -6,8 +6,15 @@ return {
     config = function()
       -- Setup orgmode
       require('orgmode').setup({
-        org_agenda_files = '~/orgfiles/**/*',
         org_default_notes_file = '~/orgfiles/refile.org',
+        org_agenda_files = '~/orgfiles/**/*',
+        org_capture_templates = {
+          n = {
+            description = "Note",
+            template = "* %?",
+            target = "~/orgfiles/notes.org",
+          }
+        },
       })
 
       -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
